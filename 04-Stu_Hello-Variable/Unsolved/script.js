d2fe5d4eb72c9609339fdfa4130aa9518a02e20e
Old Prego <spymasterluke@gmail.com>
2021-02-17 18:48:26 -0600

    Solved Variable Activity

--- 04-Stu_Hello-Variable/Unsolved/script.js
+++ 04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,17 @@
 // Write Your JavaScript Code Here
+
+var studentName = 'Luke';
+var pets = 'no';
+var funFact = 'I love 3D printing!';
+
+console.log('My name is ' + studentName);
+console.log('I have ' + pets + ' pets.');
+console.log('Fun fact: ' + funFact);
+
+studentName = 'Placeholder';
+pets = 'Placeholder';
+funFact = 'Placeholder';
+
+console.log("My partner's name is " + studentName);
+console.log("My partner has " + pets + "pets");
+console.log("A fun fact about my partner is: " + funFact);
