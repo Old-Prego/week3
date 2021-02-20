726d5d1cbfbdedc780390f8994a1de4cb1d920c1
Old Prego <spymasterluke@gmail.com>
2021-02-20 12:07:35 -0600

    updated later activities further

--- 26-Stu_Object-This/Unsolved/script.js
+++ 26-Stu_Object-This/Unsolved/script.js
@@ -2,11 +2,13 @@
 // describes the window and its properties.
 console.log(this);
 
-// describes the object win
+// describes the object window instead.
 function helloThis() {
   console.log('Inside this function, this is ' + this);
 }
 
+//Looks at the parent object and returns the age instead of having to declare
+//that you're looking at the child object instead. 
 var child = {
   age: 10,
   ageTenYears: function () {
@@ -14,6 +16,7 @@ var child = {
   },
 };
 
+//
 var investor = {
   name: 'Cash Saver',
   investment: {
@@ -24,4 +27,4 @@ var investor = {
   },
 };
 
-helloThis();
+helloThis()
